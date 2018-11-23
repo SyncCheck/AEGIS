0c02848b0c81e404fb14735434b447df227e6484
Nick <storm8472@gmail.com>
2018-11-23 12:14:29 -0700

    Started adding web requests to get word definitions when you type them in.

--- Program.cs
+++ Program.cs
@@ -10,6 +10,8 @@ using System.Collections.Generic;
 using System.Threading;
 using NetSpell;
 using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace AEGIS
 {
@@ -27,27 +29,35 @@ namespace AEGIS
         static void GetDefinition(string Word)
         {
             char FirstLetter = Word[0];
-            string html = string.Empty;
             string url = "http://www.mso.anu.edu.au/~ralph/OPTED/v003/wb1913_" + FirstLetter + ".html";
 
-            //HttpWebRequest request = (HttpWebRequest) 
+            WebClient client = new WebClient();
+            Stream stream = client.OpenRead(url);
+            StreamReader reader = new StreamReader(stream);
+            String content = StripHTML(reader.ReadToEnd());
 
-            TypeLine(url);
-        }
+
 
         
+            Console.WriteLine(content);
+        }
 
-        static void TypeLine(string line)
+        static string StripHTML(string input)
         {
-            foreach (char c in line)
+            return Regex.Replace(input, "<.*?>", String.Empty);
+        }
+
+
+
+        static void TypeLine(object obj)
+        {
+            var item = obj.ToString();
+            foreach (char c in item)
             {
                 Console.Write(c);
                 Thread.Sleep(30);
             }
         }
     }
-    
-
-    public enum WordType { Noun, Verb, Pronoun, Adjective, Abverb, Preposition, Conjunctation, Interjection }
 }
 
